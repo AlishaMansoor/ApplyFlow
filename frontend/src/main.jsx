72a61dbb54f0f923de96d36428a32b941b019577
AlishaMansoor <mansooralisha89@gmail.com>
2026-08-14 16:49:32 +0530

    added JobContext Provider in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,17 +6,20 @@ import AuthContext from './context/AuthContext.jsx'
 import UserContext from './context/UserContext.jsx'
 import SearchContext from './context/SearchContext.jsx'
 import ChatContext from './context/ChatContext.jsx'
+import JobContext from './context/JobContext.jsx'
 import axios from 'axios';
-axios.defaults.withCredentials = true; // 👈 Forces all requests to include tokens
+axios.defaults.withCredentials = true; //  Forces all requests to include tokens
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <AuthContext>
       <UserContext>
+        <JobContext>
         <ChatContext>
           <SearchContext>
             <App />
           </SearchContext>
         </ChatContext>
+        </JobContext>
       </UserContext>
     </AuthContext>
   </BrowserRouter>
