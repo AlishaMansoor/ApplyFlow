0f5cc219149c241a56d0b57d0acd669edee7bf16
AlishaMansoor <mansooralisha89@gmail.com>
2026-08-30 16:01:14 +0530

    changed ai model

--- backend/controllers/aicontrollers.js
+++ backend/controllers/aicontrollers.js
@@ -65,7 +65,7 @@ export const interviewPrep = async (req, res) => {
 
         //calling gemini
         const model = AI.getGenerativeModel({
-            model: "gemini-2.0-flash-exp",
+            model: "gemini-2.5-flash",
             generationConfig: { responseMimeType: "application/json" }
         });
 
